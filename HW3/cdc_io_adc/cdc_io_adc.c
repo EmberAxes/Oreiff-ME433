d322795a542cea75c483e893b7c59a53393f661a
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-08 17:08:01 -0500

    usb connect function
    Just put the associated setup into its own place

--- HW3/cdc_io_adc/cdc_io_adc.c
+++ HW3/cdc_io_adc/cdc_io_adc.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+
+void connectusb();
+
 int main() {
-    stdio_init_all();
-    while (!stdio_usb_connected()) {
-        sleep_ms(100);
-    }
-    printf("Start!\n");
+    
+    
  
     while (1) {
         char message[100];
@@ -13,4 +13,12 @@ int main() {
         printf("message: %s\r\n",message);
         sleep_ms(50);
     }
+}
+
+void connectusb(){
+    stdio_init_all();
+    while (!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
+    printf("Start!\n");
 }
