7287a9aa4d765140f728c773a17552c8fd9ebc77
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-08 17:12:05 -0500

    gpio initialize test
    Setting up a simple button and led circuit

--- HW3/cdc_io_adc/cdc_io_adc.c
+++ HW3/cdc_io_adc/cdc_io_adc.c
@@ -5,13 +5,18 @@ void connectusb();
 
 int main() {
     
-    
+    gpio_init(0);       // initializing pin GP0 (pin 1 on pi)
+    gpio_init(1);       // initialization pin GP1 (pin 2 on pi)
+
+    gpio_set_dir(0,GPIO_IN);    // for a button input, default state is 1
+    gpio_set_dir(1,GPIO_OUT);   // for an LED output
  
     while (1) {
-        char message[100];
-        scanf("%s", message);
-        printf("message: %s\r\n",message);
-        sleep_ms(50);
+        if (gpio_get(0) == 0){      // if the button is pressed
+            gpio_put(1,1);          // turn led on
+            sleep_ms(100);          // wait a moment
+        }
+        gpio_put(1,0);              // LED is off otherwise
     }
 }
 
