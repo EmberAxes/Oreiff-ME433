2c969755ed5c189f4497b485a5b8ef93fbc734bf
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-08 17:03:49 -0500

    USB connect test
    Checking print does not occur until Putty is open

--- HW3/cdc_io_adc/cdc_io_adc.c
+++ HW3/cdc_io_adc/cdc_io_adc.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
-
-int main()
-{
+int main() {
     stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    while (!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
+    printf("Start!\n");
+ 
+    while (1) {
+        char message[100];
+        scanf("%s", message);
+        printf("message: %s\r\n",message);
+        sleep_ms(50);
     }
 }
