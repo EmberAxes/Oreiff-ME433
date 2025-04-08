49a279cd2550ff16f352a8c6ed7127de526191a4
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-08 17:21:41 -0500

    Initializing adc
    Getting button and putty ready to read adc pin

--- HW3/cdc_io_adc/cdc_io_adc.c
+++ HW3/cdc_io_adc/cdc_io_adc.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 
 void connectusb();
 
 int main() {
     
-    gpio_init(0);       // initializing pin GP0 (pin 1 on pi)
-    gpio_init(1);       // initialization pin GP1 (pin 2 on pi)
-
-    gpio_set_dir(0,GPIO_IN);    // for a button input, default state is 1
-    gpio_set_dir(1,GPIO_OUT);   // for an LED output
- 
-    while (1) {
-        if (gpio_get(0) == 0){      // if the button is pressed
-            gpio_put(1,1);          // turn led on
-            sleep_ms(100);          // wait a moment
-        }
-        gpio_put(1,0);              // LED is off otherwise
-    }
+    
 }
 
 void connectusb(){
@@ -27,3 +16,17 @@ void connectusb(){
     }
     printf("Start!\n");
 }
+
+// gpio_init(0);       // initializing pin GP0 (pin 1 on pi)
+//     gpio_init(1);       // initialization pin GP1 (pin 2 on pi)
+
+//     gpio_set_dir(0,GPIO_IN);    // for a button input, default state is 1
+//     gpio_set_dir(1,GPIO_OUT);   // for an LED output
+ 
+//     while (1) {
+//         if (gpio_get(0) == 0){      // if the button is pressed
+//             gpio_put(1,1);          // turn led on
+//             sleep_ms(100);          // wait a moment
+//         }
+//         gpio_put(1,0);              // LED is off otherwise
+//     }
