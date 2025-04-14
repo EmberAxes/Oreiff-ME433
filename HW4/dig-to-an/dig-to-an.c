dd8cf06d08e68d7c77bec83604cf6ccca1a1c959
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-14 11:51:19 -0500

    Pins and functions
    Initializing pins and functions

--- HW4/dig-to-an/dig-to-an.c
+++ HW4/dig-to-an/dig-to-an.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include <math.h>
 
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
@@ -11,18 +12,20 @@
 #define PIN_SCK  18
 #define PIN_MOSI 19
 
-
+static inline void cs_select(uint cs_pin);
+static inline void cs_deselect(uint cs_pin);
+void writeDac();
+void inwriteDac(int channel, float voltage);
 
 int main()
 {
     stdio_init_all();
 
     // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    spi_init(spi_default, 1000 * 1000); // the baud, or bits per second
+    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
     
     // Chip select is active-low, so we'll initialise it to a driven-high state
     gpio_set_dir(PIN_CS, GPIO_OUT);
@@ -33,4 +36,16 @@ int main()
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+}
+
+static inline void cs_select(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
 }
