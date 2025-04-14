1be3e98e091c5b1d3946b0f9d14b62a9512a25da
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-14 13:51:40 -0500

    Testing write Dac
    First try of bitshifting stuff

--- HW4/dig-to-an/dig-to-an.c
+++ HW4/dig-to-an/dig-to-an.c
@@ -33,10 +33,15 @@ int main()
     gpio_put(PIN_CS, 1);
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
+    uint8_t data[2];
+
     while (true) {
         printf("Hello, world!\n");
-        writeDac();
+        
+        inwriteDac(0, 1023);    // Testing channel A output 3.3 V
+        writeDac(data);
         sleep_ms(1);
+
     }
 }
 
@@ -53,25 +58,29 @@ static inline void cs_deselect(uint cs_pin) {
 }
 
 // Write to pin
-void writeDac(){
-    uint8_t data[2];
+void writeDac(uint8_t data[2]){
     int len = 2;
-    data[0] = 0b01111010;
-    data[1] = 255;
     cs_select(PIN_CS);
     spi_write_blocking(SPI_PORT, data, len); // where data is a uint8_t array with length len
     cs_deselect(PIN_CS);
 }
 
-// // Step 1: Function that takes channel and voltage [0 - 1023] as inputs
-// void inwriteDac(int channel, float voltage){
-//     uint8_t data[2];    // Have to put everything in here eventually
-//     uint16_t d = 0;
+// Step 1: Function that takes channel and voltage [0 - 1023] as inputs
+void inwriteDac(int channel, float voltage){
+    uint8_t data[2];    // Have to put everything in here eventually
+    int len = 2;
+    uint16_t d = 0;
 
-//     // Need to put the 16 bit number in the right spot
-//     d = d | (channel << 15);    // bit shifting to choose output A
-//     d = d | 0b111 << 12;        // we've been told the next bits are all 1
+    // Need to put the 16 bit number in the right spot
+    d = d | (channel << 15);    // bit shifting to choose output A
+    d = d | 0b111 << 12;        // we've been told the next bits are all 1
 
+    // Voltage stuff
+    uint16_t v = (voltage * 1024) / 1023;
+    d = d | v << 2;     // Need the last 2 bits to be zero
+    data[0] = d >> 8;   // Grab the 4 channel stuff and first 4 voltage bits
+    data[1] = d & 0b11111111; // set the final 8 bits;
+}
 
 // }
  
