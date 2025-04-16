05c7c8a40c7697937825908676295d89fbde35b9
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-16 12:14:03 -0500

    Read address/instruction
    Initialized the RAM reading

--- HW5/math-time-ram/math-time-ram.c
+++ HW5/math-time-ram/math-time-ram.c
@@ -13,34 +13,28 @@
 
 #define RAM_CS 15
 
+union FloatInt{
+    float f;
+    uint32_t i;
+};
+
+static inline void cs_select(uint cs_pin);
+static inline void cs_deselect(uint cs_pin);
+
 void spi_pi_init();
 void spi_ram_init();
-
-static inline void cs_select(uint cs_pin) {
-    asm volatile("nop \n nop \n nop"); // FIXME
-    gpio_put(cs_pin, 0);
-    asm volatile("nop \n nop \n nop"); // FIXME
-}
-
-static inline void cs_deselect(uint cs_pin) {
-    asm volatile("nop \n nop \n nop"); // FIXME
-    gpio_put(cs_pin, 1);
-    asm volatile("nop \n nop \n nop"); // FIXME
-}
+float read_ram(uint16_t address);
+void write_ram(uint16_t address, float data);
 
 
 int main()
 {
+    //initialization chunk
     stdio_init_all();
-
     spi_pi_init();
+    spi_ram_init();
     
-    // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    
 }
 
 void spi_pi_init(){
@@ -69,5 +63,34 @@ void spi_ram_init(){
 
     cs_select(RAM_CS);
     spi_write_blocking(SPI_PORT, buff, 2); // where data is a uint8_t array with length len
+    cs_deselect(RAM_CS);
+}
+
+static inline void cs_select(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+float read_ram(uint16_t address){
+    uint8_t out_buff[7], in_buff[7];
+    out_buff[0] = 0b00000011;       // Read instruction
+    out_buff[1] = address >> 8;     // First 8 bits of address
+    out_buff[2] = address & 0xFF;   // Last 8 bits of address
+
+    out_buff[3] = 0;
+    out_buff[4] = 0;
+    out_buff[5] = 0;
+    out_buff[6] = 0;
+    out_buff[7] = 0;
+
+    cs_select(RAM_CS);
+    spi_write_read_blocking(spi_default, out_buff, in_buff, 7);
     cs_deselect(RAM_CS);
 }
