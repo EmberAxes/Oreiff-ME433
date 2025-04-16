63af50891658a9f14092589359f5e1030ea7f10a
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-16 11:43:24 -0500

    ram initialization
    Initialization functions created

--- HW5/math-time-ram/math-time-ram.c
+++ HW5/math-time-ram/math-time-ram.c
@@ -7,7 +7,7 @@
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define SPI_PORT spi0
 #define PI_RX 16
-#define PI_CS   17
+#define DAC_CS   17
 #define PI_SCK  18
 #define PI_TX 19
 
@@ -15,6 +15,19 @@
 
 void spi_pi_init();
 void spi_ram_init();
+
+static inline void cs_select(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
 
 int main()
 {
@@ -35,7 +48,7 @@ void spi_pi_init(){
 
     // These are for the DAC
     gpio_set_function(PI_RX, GPIO_FUNC_SPI);
-    gpio_set_function(PI_CS,   GPIO_FUNC_SIO);
+    gpio_set_function(DAC_CS,   GPIO_FUNC_SIO);
     gpio_set_function(PI_SCK,  GPIO_FUNC_SPI);
     gpio_set_function(PI_TX, GPIO_FUNC_SPI);
 
@@ -43,7 +56,18 @@ void spi_pi_init(){
     gpio_set_function(RAM_CS, GPIO_FUNC_SIO);
     
     // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PI_CS, GPIO_OUT);
-    gpio_put(PI_CS, 1);
+    gpio_set_dir(DAC_CS, GPIO_OUT);
+    gpio_put(DAC_CS, 1);
+    gpio_set_dir(RAM_CS, GPIO_OUT);
+    gpio_put(RAM_CS, 1);
 }
 
+void spi_ram_init(){
+    uint8_t buff[2];
+    buff[0] = 0b00000101;    // Change status register
+    buff[1] = 0b01000000;    // to sequential mode
+
+    cs_select(RAM_CS);
+    spi_write_blocking(SPI_PORT, buff, 2); // where data is a uint8_t array with length len
+    cs_deselect(RAM_CS);
+}
