024bb2ed989d113b1bcbed4a192a27fc6af556d5
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-17 00:56:50 -0500

    Cycles calculations
    It's wrong, the math is wrong

--- HW5/timing/timing/timing.c
+++ HW5/timing/timing/timing.c
@@ -26,32 +26,32 @@ int main()
     printf("Cycles = %llu\n", cycles);
 
     // Subtraction -------------------------------------------
-    absolute_time_t t1 = get_absolute_time();
+    t1 = get_absolute_time();
     for (int i = 0; i < 1000; i++) {
         f_sub = f1 - f2;
     }
-    uint64_t t = to_us_since_boot(t1)/1000;
-    uint64_t cycles = t/667;
+    t = to_us_since_boot(t1)/1000;
+    cycles = t/667;
     printf("Difference = %f\r\n",f_sub);
     printf("Cycles = %llu\n", cycles);
 
     // Multiplication -----------------------------------------
-    absolute_time_t t1 = get_absolute_time();
+    t1 = get_absolute_time();
     for (int i = 0; i < 1000; i++) {
         f_mult = f1 * f2;
     }
-    uint64_t t = to_us_since_boot(t1)/1000;
-    uint64_t cycles = t/667;
+    t = to_us_since_boot(t1)/1000;
+    cycles = t/667;
     printf("Product = %f\r\n",f_mult);
     printf("Cycles = %llu\n", cycles);
 
     // Division ------------------------------------------------
-    absolute_time_t t1 = get_absolute_time();
+    t1 = get_absolute_time();
     for (int i = 0; i < 1000; i++) {
         f_div = f1 / f2;
     }
-    uint64_t t = to_us_since_boot(t1)/1000;
-    uint64_t cycles = t/667;
+    t = to_us_since_boot(t1)/1000;
+    cycles = t/667;
     printf("Product = %f\r\n",f_div);
     printf("Cycles = %llu\n", cycles);
     
