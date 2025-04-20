1173e8468564d67c5fcfef9ee60bc05725e88d1f
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-20 18:24:19 -0500

    GP7 and GP0 initialized
    Set GP7 to output (0) and GP0 to input (1) along with everything else as an input

--- HW6/i2c-io/i2c-io.c
+++ HW6/i2c-io/i2c-io.c
@@ -9,7 +9,8 @@
 #define I2C_SDA 8
 #define I2C_SCL 9
 
-void heartbeat();
+void setPin(unsigned char addr, unsigned char reg, unsigned char val);
+unsigned char readPin(unsigned char address, unsigned char register);
 
 int main()
 {
@@ -24,10 +25,17 @@ int main()
     gpio_pull_up(I2C_SCL);
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
+    // Heartbeat initialized
     gpio_init(25);
     gpio_set_dir(25,GPIO_OUT);
+
+    // GP7 out and GP0 in initialization
+    setPin(0, 0x00, 0b01111111);  // This should initialize
+                                  // GP0 as input and GP7 as output
+
     while (true) {
-        printf("Hello, world!\n");
+        
+        // Heartbeat
         gpio_put(25,1);
         sleep_ms(250);
         gpio_put(25,0);
@@ -35,3 +43,14 @@ int main()
     }
 }
 
+void setPin(unsigned char addr, unsigned char reg, unsigned char val){
+    unsigned char buff[2];
+    unsigned char address = 0;
+    address = (1 << 7) | (addr << 1);  // 0 1 0 0 a d r 0  The 0 means write
+    buff[0] = reg;
+    buff[1] = val;
+
+    gpio_put(I2C_SCL, 0);       // Set SCL low
+    i2c_write_blocking(i2c_default, addr, buff, 2, false);
+    gpio_put(I2C_SCL, 1);       // set back to high
+}
