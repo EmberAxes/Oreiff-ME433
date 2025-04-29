7fc67faaff1cb3d970bfb9805f7df3873e6d2e38
EmberAxes <olivia.s.reiff@outlook.com>
2025-04-28 19:39:05 -0500

    one LED color cycle
    Make one led shift through all the colors

--- HW8/pwm-combo/pio_ws2812/ws2812.c
+++ HW8/pwm-combo/pio_ws2812/ws2812.c
@@ -58,6 +58,8 @@ typedef struct {
     unsigned char b;
 } wsColor; 
 
+wsColor HSBtoRGB(float hue, float sat, float brightness);
+
 int main() {
     //set_sys_clock_48();
     stdio_init_all();
@@ -77,15 +79,93 @@ int main() {
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
     wsColor c;
-    c.r = 255;
-    c.g = 0;
-    c.b = 0;
+    int i,j;
+    j = 0;
 
     while (1) {
-        int i;
+        if (j==360){
+            j = 0;
+        }
+        c = HSBtoRGB(j,1,0.1);
         for(i=0;i<NUM_PIXELS;i++){
             put_pixel(pio, sm, urgb_u32(c.r, c.g, c.b)); // assuming you've made arrays of colors to send
         }
+        j++;
         sleep_ms(1); // wait at least the reset time
     }
+}
+
+// adapted from https://forum.arduino.cc/index.php?topic=8498.0
+// hue is a number from 0 to 360 that describes a color on the color wheel
+// sat is the saturation level, from 0 to 1, where 1 is full color and 0 is gray
+// brightness sets the maximum brightness, from 0 to 1
+wsColor HSBtoRGB(float hue, float sat, float brightness) {
+    float red = 0.0;
+    float green = 0.0;
+    float blue = 0.0;
+
+    if (sat == 0.0) {
+        red = brightness;
+        green = brightness;
+        blue = brightness;
+    } else {
+        if (hue == 360.0) {
+            hue = 0;
+        }
+
+        int slice = hue / 60.0;
+        float hue_frac = (hue / 60.0) - slice;
+
+        float aa = brightness * (1.0 - sat);
+        float bb = brightness * (1.0 - sat * hue_frac);
+        float cc = brightness * (1.0 - sat * (1.0 - hue_frac));
+
+        switch (slice) {
+            case 0:
+                red = brightness;
+                green = cc;
+                blue = aa;
+                break;
+            case 1:
+                red = bb;
+                green = brightness;
+                blue = aa;
+                break;
+            case 2:
+                red = aa;
+                green = brightness;
+                blue = cc;
+                break;
+            case 3:
+                red = aa;
+                green = bb;
+                blue = brightness;
+                break;
+            case 4:
+                red = cc;
+                green = aa;
+                blue = brightness;
+                break;
+            case 5:
+                red = brightness;
+                green = aa;
+                blue = bb;
+                break;
+            default:
+                red = 0.0;
+                green = 0.0;
+                blue = 0.0;
+                break;
+        }
+    }
+
+    unsigned char ired = red * 255.0;
+    unsigned char igreen = green * 255.0;
+    unsigned char iblue = blue * 255.0;
+
+    wsColor c;
+    c.r = ired;
+    c.g = igreen;
+    c.b = iblue;
+    return c;
 }
