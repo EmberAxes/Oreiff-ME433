820ca379dd22cb3bf514c9b5990b6d39b6c3e334
EmberAxes <olivia.s.reiff@outlook.com>
2025-05-03 15:23:01 -0500

    Q1 complete
    Making csv files

---
+++ HW10/signal_processing.py
@@ -0,0 +1,25 @@
+# Question 1: Get data into lists
+import csv
+
+def make_csv(file):
+
+    t = [] # column 0
+    data1 = [] # column 1
+
+    with open(file) as f:
+        # open the csv file
+        reader = csv.reader(f)
+        for row in reader:
+            # read the rows 1 one by one
+            t.append(float(row[0])) # leftmost column
+            data1.append(float(row[1])) # second column
+
+    # print the data to verify it was read
+    print(len(t), ", ",len(data1))
+    
+    return (t, data1)
+
+tA,sA = make_csv("sigA.csv")
+tB,sB = make_csv("sigB.csv")
+tC,sC = make_csv("sigC.csv")
+tD,sD = make_csv("sigD.csv")
